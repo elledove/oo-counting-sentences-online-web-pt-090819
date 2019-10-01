263806f70feed6f010b0755c971bd6bda0f407b2
Lauren Dove <elledoveinc@gmail.com>
2019-10-01 02:18:46 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ self.end_with?("!")
   end
 
   def count_sentences
-    self.split(/[.?!]/).count.reject{|n| n.empty?}
+    self.split(/[.?!]/).count
 #self.split(/\W+/).count
 #binding.pry
   end
